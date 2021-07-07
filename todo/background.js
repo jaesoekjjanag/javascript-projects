0cb6231501be110a7ff24aa4a691d43a379e280d
jaesoekjjanag <78065948+jaesoekjjanag@users.noreply.github.com>
2021-07-07 20:43:42 +0900

    Update background.js

--- todo/background.js
+++ todo/background.js
@@ -3,7 +3,8 @@ const background = document.querySelector('#background');
 let backgroundImg = [
   'image1.jpg',
   'image2.jpg',
-  'image3.jpg'
+  'image3.jpg',
+  'image4.jpg'
 ]
 
 const randomBg = backgroundImg[Math.floor(Math.random() * backgroundImg.length)];
